152e526a998b3b6305169d687801a1342c28038b
mayman <m.ayman@tamkeentech.sa>
2022-10-26 13:24:42 +0300

    first commit

---
+++ src/test/java/com/anchtun/JavaDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anchtun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
