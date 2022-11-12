e8cab3b7269ae2174ea2184264837042073c7bbc
mayman <m.ayman@tamkeentech.sa>
2022-11-12 10:29:45 +0300

    object pool

--- src/main/java/com/anchtun/designpatterns/creational/objectpool/client/Client.java
+++ src/main/java/com/anchtun/designpatterns/creational/objectpool/client/Client.java
@@ -7,18 +7,26 @@ import javafx.geometry.Point2D;
 
 public class Client {
 
-	public static final ObjectPool<Bitmap> bitmapPool = new ObjectPool<>(() -> new Bitmap("Mohamed-profile.bmp"), 5);
+	public static final ObjectPool<Bitmap> bitmapPool = new ObjectPool<>(() -> new Bitmap("Mohamed-profile.bmp"), 3);
 
 	public static void main(String[] args) {
 		Bitmap b1 = bitmapPool.get();
 		b1.setLocation(new Point2D(12, 12));
 		Bitmap b2 = bitmapPool.get();
 		b2.setLocation(new Point2D(-12, 0));
+		Bitmap b3 = bitmapPool.get();
+		b3.setLocation(new Point2D(-20, 20));
+		//Bitmap b4 = bitmapPool.get();
+		//b4.setLocation(new Point2D(-5, 0));
 
 		b1.draw();
 		b2.draw();
+		b3.draw();
+		//b4.draw();
 
 		bitmapPool.release(b1);
 		bitmapPool.release(b2);
+		bitmapPool.release(b3);
+		//bitmapPool.release(b4);
 	}
 }
