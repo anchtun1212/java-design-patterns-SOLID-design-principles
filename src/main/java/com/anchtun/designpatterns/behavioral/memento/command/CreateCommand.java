8682a873b481853e19bfd3d87f4ee5c65873cc33
mayman <m.ayman@tamkeentech.sa>
2022-12-07 20:08:35 +0300

    mememto

---
+++ src/main/java/com/anchtun/designpatterns/behavioral/memento/command/CreateCommand.java
@@ -0,0 +1,20 @@
+package com.anchtun.designpatterns.behavioral.memento.command;
+
+import com.anchtun.designpatterns.behavioral.memento.WorkflowDesigner;
+
+public class CreateCommand extends AbstractWorkflowCommand {
+
+	private String name;
+
+	public CreateCommand(WorkflowDesigner designer, String name) {
+		super(designer);
+		this.name = name;
+	}
+
+	@Override
+	public void execute() {
+		this.memento = receiver.getMemento();
+		receiver.createWorkflow(name);
+	}
+
+}
---
+++ src/main/java/com/anchtun/designpatterns/behavioral/memento/command/WorkflowCommand.java
@@ -0,0 +1,8 @@
+package com.anchtun.designpatterns.behavioral.memento.command;
+
+public interface WorkflowCommand {
+
+	void execute();
+
+	void undo();
+}
